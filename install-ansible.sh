7e70fe0a7bc5a00cad9a1ad193c7013173cdbf32
Sarada Symonds <ssymonds@wmb37-38e.broadinstitute.org>
2020-02-20 13:32:45 -0500

    initialize ansible role

--- install-ansible.sh
+++ install-ansible.sh
@@ -5,7 +5,7 @@ apt-get -y update && apt-get -y upgrade
 apt-get install -y curl
 apt-get install -y python3 python3-pip
 pip3 install paramiko
-pip3 install 'ansible==2.4.2.0'
+pip3 install "ansible==2.7.0.0"
 
 # Installs the most updated version of ansible 
 # echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' >> /etc/apt/sources.list
@@ -14,5 +14,4 @@ pip3 install 'ansible==2.4.2.0'
 #apt install -y ansible
 
 apt-get install -y git
-ansible --version
 ansible-galaxy install -p roles -r requirements.yml
